f6a0375be7d7493673883b6793cb1f23e80c4eb3
--unset <--unset>
2021-11-21 19:18:06 -0500

    creacion CRUD, pruebas con postman con base de datos local

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -8,8 +8,30 @@ router.get('/', async (req, res) => {
     res.json(tasks);
 });
 
+router.get('/:id', async (req, res) => {
+    const tasks = await Task.findById(req.params.id);    
+    res.json(tasks);
+});
+
+
 router.post('/', async (req, res) => {
-    console.log(req.body);
+    const {title, description} = req.body;
+    const task = new Task({title, description});
+    //console.log(task);
+    await task.save();
+    res.json({status: 'tarea guardada'});
+});
+
+router.put('/:id', async (req, res) => {
+    const {title, description} = req.body;
+    const newTask = {title, description};
+    await Task.findByIdAndUpdate(req.params.id, newTask);
+    res.json({status: 'tarea actualizada'});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({status: 'tarea eliminad'});
 });
 
 module.exports = router;
